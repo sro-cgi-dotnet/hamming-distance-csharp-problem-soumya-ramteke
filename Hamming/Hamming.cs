8412840ebea23d72a8868dac44081b4710e99f15
soumya-ramteke <soumya.r@cgi.com>
2018-09-11 09:55:03 +0530

    first assgnmnt .net

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -6,9 +6,29 @@ namespace Hamming
     {
         public static int Distance(string original, string current)
         {
-            int hammingDistance = 0;
-            
-            return hammingDistance;
+        int hammingDistance=0, i=0;
+        if(original==null)
+        {
+            throw new ArgumentNullException("original");
+        }
+        else if(current==null)
+        {
+            throw new ArgumentNullException("current");
+        }
+        else if(original.Length!=current.Length)
+        {
+            throw new ArgumentException("Hamming Distance can only be calculated over strings of equal length");
+        }
+        else{
+        for(i=0; i<original.Length; i++)
+        {
+            if(original[i]!=current[i])
+            {
+                hammingDistance++;
+            }
+        }
+        }
+        return hammingDistance;
         }
     }
 }
